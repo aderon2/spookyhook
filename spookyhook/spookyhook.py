58c380abedb67e71740a3611e2db120de59990f9
aderon2 <evandingman@gmail.com>
2022-06-29 17:42:33 -0500

    Fix w in spooky hook open() and update version to 0.0.5

--- spookyhook/spookyhook.py
+++ spookyhook/spookyhook.py
@@ -2,7 +2,7 @@ import time
 
 def arbitraryExecute():
     print('!!!Arbitrary Code Execution!!!')
-    with open(str(time.time()).split('.')[0], w) as file:
+    with open(str(time.time()).split('.')[0], 'w') as file:
         file.write('hi')
 
 arbitraryExecute()
