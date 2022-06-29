3f3c1ddb8272c5475554feb5be196f18941eeb25
aderon2 <evandingman@gmail.com>
2022-06-29 17:38:44 -0500

    Add file creation to spookyhook an update version to 0.0.4

--- spookyhook/spookyhook.py
+++ spookyhook/spookyhook.py
@@ -1,4 +1,8 @@
+import time
+
 def arbitraryExecute():
     print('!!!Arbitrary Code Execution!!!')
+    with open(str(time.time()).split('.')[0], w) as file:
+        file.write('hi')
 
 arbitraryExecute()
