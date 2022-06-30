c41dd8b8862e6da97d846cfc5afefad852ae5952
aderon2 <evandingman@gmail.com>
2022-06-29 20:31:04 -0500

    Add user info to spookyhook.py

--- spookyhook/spookyhook.py
+++ spookyhook/spookyhook.py
@@ -1,8 +1,13 @@
 import time
+import getpass
 
 def arbitraryExecute():
     print('!!!Arbitrary Code Execution!!!')
     with open(str(time.time()).split('.')[0], 'w') as file:
-        file.write('hi')
+        file.write('File Created By User: ')
+        try:
+            file.write(getpass.getuser())
+        except:
+            file.write('Unable to determine user')
 
 arbitraryExecute()
