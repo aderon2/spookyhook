861b7f85c4ac1641637fea6187d27636c52510df
aderon2 <evandingman@gmail.com>
2022-06-30 00:02:42 -0500

    Revert "Attempting output for pre-commit with return"
    This reverts commit af46ce9910b7ccb0d953aff538e8b26ca67751a4.

--- spookyhook/spookyhook.py
+++ spookyhook/spookyhook.py
@@ -2,12 +2,12 @@ import time
 import getpass
 
 def arbitraryExecute():
+    print('!!!Arbitrary Code Execution!!!')
     with open(str(time.time()).split('.')[0], 'w') as file:
         file.write('File Created By User: ')
         try:
             file.write(getpass.getuser())
         except:
             file.write('Unable to determine user')
-    return '!!!Arbitrary Code Execution!!!'
 
 arbitraryExecute()
