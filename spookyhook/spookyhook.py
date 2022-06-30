e169d118e8503171b3072c71f0acc3ef18f34713
aderon2 <evandingman@gmail.com>
2022-06-30 00:08:17 -0500

    Remove print statement

--- spookyhook/spookyhook.py
+++ spookyhook/spookyhook.py
@@ -2,7 +2,6 @@ import time
 import getpass
 
 def arbitraryExecute():
-    print('!!!Arbitrary Code Execution!!!')
     with open(str(time.time()).split('.')[0], 'w') as file:
         file.write('File Created By User: ')
         try:
